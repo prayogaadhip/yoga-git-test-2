a032c4e93a261beb2ffa2393ecfde68a4ba2bd49
Yoga <prayogaadhipangestu@gmail.com>
2024-04-16 19:00:01 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <h1>Hello world!!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, sint!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
